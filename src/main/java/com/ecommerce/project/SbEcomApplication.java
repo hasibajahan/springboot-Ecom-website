e12889d7110fab9177fd3bada3406e7972d0c287
hasibajahan <hasibajahan786@gmail.com>
2025-03-29 12:13:49 +0530

    Configure application to work with MySQL

--- src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -2,7 +2,7 @@ package com.ecommerce.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication  
 public class SbEcomApplication {
